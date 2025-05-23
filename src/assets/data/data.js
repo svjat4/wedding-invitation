3f5b13094fc84f4110a677661417078cb9eaf198
lazyfay <rifaiarsaa@gmail.com>
2025-05-23 13:02:20 +0700

    wedding invitation V1

---
+++ src/assets/data/data.js
@@ -0,0 +1,141 @@
+export const data = {
+    bride: {
+        L: {
+            id: 1,
+            name: 'Andi Setioko <br>(Dispenduk Capil)',
+            child: 'Putra ke 1',
+            father: 'Bapak Sunaryo (Satpol PP)',
+            mother: 'Rubingatun',
+            image: './src/assets/images/cowo.png'
+        },
+        P: {
+            id: 2,
+            name: 'Elya Putri Erista Novita Sari',
+            child: 'Putri ke 1',
+            father: 'Alm. Bapak Agus Sugihardi',
+            mother: 'Endang Prihatin',
+            image: './src/assets/images/cewe.png'
+        },
+
+        couple: './src/assets/images/couple.png',
+        jawa: './src/assets/images/gunungan.webp'
+    },
+
+    time: {
+        marriage: {
+            year: '2025',
+            month: 'Agustus',
+            date: '06',
+            day: 'Rabu',
+            hours: {
+                start: '07.00',
+                finish: 'Selesai'
+            }
+        },
+        temumanten: {
+            year: '2025',
+            month: 'Agustus',
+            date: '06',
+            day: 'Rabu',
+            hours: {
+                start: '09.00',
+                finish: 'Selesai'
+            }
+        },   
+        reception: {
+            year: '2025',
+            month: 'Agustus',
+            date: '06',
+            day: 'Rabu',
+            hours: {
+                start: '13.00',
+                finish: 'Selesai'
+            }
+        },
+        address: 'Jl. Jawa no.60 RT 02 RW 04, Kel.Kepolorejo, Kec. Magetan, Kab. Magetan, Jawa Timur'
+    },
+
+    link: {
+        calendar: 'https://calendar.app.google/QcGapfvCMe9PWk4A9',
+        map: 'https://maps.app.goo.gl/ErPW7YZndo2CXQHu6',
+    },
+
+    galeri: [
+        {
+            id: 1,
+            image: './src/assets/images/1.jpg'
+        },
+        {
+            id: 2,
+            image: './src/assets/images/2.jpg'
+        },
+        {
+            id: 3,
+            image: './src/assets/images/3.jpg'
+        },
+        {
+            id: 4,
+            image: './src/assets/images/4.jpg'
+        },
+        {
+            id: 5,
+            image: './src/assets/images/5.jpg'
+        },
+        {
+            id: 5,
+            image: './src/assets/images/6.jpg'
+        }
+    ],
+
+    bank: [
+        {
+            id: 1,
+            name: 'Lorem Ipsum',
+            icon: './src/assets/images/bca.png',
+            rekening: '12345678'
+        },
+        {
+            id: 2,
+            name: 'Elya Putri Erista Novita Sari',
+            icon: './src/assets/images/bri.png',
+            rekening: '636301018175532'
+        },
+    ],
+
+    audio: './src/assets/audio/caknan.mp3',
+
+    api: 'https://script.google.com/macros/s/AKfycbxq3CiAUsNVF6h5-3C9aB91t1i7Tav2ci9AZ98dw--p0RTLF0t2d-JAoOoE1b-lqnh5RA/exec',
+
+    navbar: [
+        {
+            id: 1,
+            teks: 'Home',
+            icon: 'bx bxs-home-heart',
+            path: '#home',
+        },
+        {
+            id: 2,
+            teks: 'Mempelai',
+            icon: 'bx bxs-group',
+            path: '#bride',
+        },
+        {
+            id: 3,
+            teks: 'Tanggal',
+            icon: 'bx bxs-calendar-check',
+            path: '#time',
+        },
+        {
+            id: 4,
+            teks: 'Galeri',
+            icon: 'bx bxs-photo-album',
+            path: '#galeri',
+        },
+        {
+            id: 5,
+            teks: 'Ucapan',
+            icon: 'bx bxs-message-rounded-dots',
+            path: '#wishas',
+        },
+    ],
+}
---
+++ src/js/time.js
@@ -0,0 +1,21 @@
+import { data } from "../assets/data/data.js";
+
+export const time = () => {
+    const timeContainer = document.querySelector('.time');
+    const [marriageDiv, temumantenDiv, receptionDiv] = timeContainer.querySelectorAll('div div');
+    const mapLink = timeContainer.querySelector('a');
+    const addressParagraph = timeContainer.querySelector('a + p');
+
+    const createTimeListItem = (title, details) => (
+        `<h3>${title}</h3>
+         <p>${details.day}, ${details.date} ${details.month} ${details.year} <br> 
+         Pukul ${details.hours.start} WIB sd ${details.hours.finish}</p>`
+    );
+
+    marriageDiv.innerHTML = createTimeListItem('Akad', data.time.marriage);
+    temumantenDiv.innerHTML = createTimeListItem('Temu Manten', data.time.temumanten);
+    receptionDiv.innerHTML = createTimeListItem('Resepsi', data.time.reception);
+
+    mapLink.href = data.link.map;
+    addressParagraph.textContent = data.time.address;
+};
